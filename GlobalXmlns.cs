3f340988f823765931fd1c8960914166232356f5
Tt3o <105876437+Mateocorone@users.noreply.github.com>
2025-12-15 08:53:33 -0500

    Add files via upload

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Farmacia_V_M.R.E.A_")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Farmacia_V_M.R.E.A_.Pages")]
