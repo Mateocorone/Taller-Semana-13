6fa9e76bdcef99900ea13d61a3fbad3193e095f3
Tt3o <mateocoronel79@gmail.com>
2025-12-15 09:03:23 -0500

    Revert "Add files via upload"
    This reverts commit 3f340988f823765931fd1c8960914166232356f5.

--- GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Farmacia_V_M.R.E.A_")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Farmacia_V_M.R.E.A_.Pages")]
